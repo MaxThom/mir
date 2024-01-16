21d51763a097abc279af8db26b0b81b99c37c49a
maxthom <maxthomassin@hotmail.com>
2024-01-16 23:05:13 +1300

    added jetstream example

--- cmds/protoproxy/main.go
+++ cmds/protoproxy/main.go
@@ -15,9 +15,11 @@ import (
 	"github.com/maxthom/mir/libs/boiler/mir_config"
 	"github.com/maxthom/mir/libs/boiler/mir_log"
 	"github.com/maxthom/mir/libs/boiler/mir_signals"
+	bus "github.com/maxthom/mir/libs/external/natsio"
 	proto_store "github.com/maxthom/mir/libs/proto/store"
 	"github.com/maxthom/mir/services/protoproxy"
 	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
 	logger "github.com/rs/zerolog/log"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
@@ -64,9 +66,16 @@ type (
 )
 
 // TODO
-//   - Library to translate proto to influx
-//   - The api could have an endpoint to send telemetry as json
-//   - The api could have an endpoint to send telemetry as grpc with a dynamic grpc server
+//   - [x] Library to translate proto to influx
+//   - [x] The api could have an endpoint to send telemetry as json
+//   - [x] The api could have an endpoint to send telemetry as grpc with a dynamic grpc server
+//   - [x] Define route mechanism
+//   - [x] Setup Nats
+//   - [ ] Setup QuestDB
+//   - [ ] Pipe telemetry from Nats to QuestDB using protobuf to line protocol
+//   - [ ] Create server side Library
+//   - [ ] Create client side Library
+//   - [ ] Create the swarm
 func main() {
 	mir_signals.Notify(syscall.SIGHUP, syscall.SIGINT, syscall.SIGQUIT)
 	mux := http.NewServeMux()
@@ -94,16 +103,73 @@ func main() {
 	//      in the future, this could be an interface to many store type
 
 	// Connect to a server
-	nc, err := nats.Connect(cfg.DataBusServer.Url)
+	b, err := bus.New(cfg.DataBusServer.Url,
+		bus.WithReconnHandler(func(nc *nats.Conn) {
+			logger.Warn().Msg("reconnected to " + nc.ConnectedUrl())
+		}),
+		bus.WithDisconnHandler(func(nc *nats.Conn, err error) {
+			logger.Warn().Msg(fmt.Sprintf("disconnected due to %v, will attempt to reconnect ", err))
+		}),
+		bus.WithClosedHandler(func(nc *nats.Conn) {
+			logger.Warn().Msg("connection to %v closed " + nc.ConnectedUrl())
+		}))
 	if err != nil {
 		logger.Err(err).Msg("")
 	}
-	// Simple Async Suubscriber
-	nc.Subscribe("foo", func(m *nats.Msg) {
-		fmt.Printf("Received a message: %s\n", string(m.Data))
-		nc.Publish(m.Reply, []byte("I can help!"))
-	})
+	logger.Info().Msg("connected to msg bus on " + cfg.DataBusServer.Url)
 
+	// TODO
+	// - [ ] worker group on the queue
+	js, _ := jetstream.New(b.Conn)
+	ctxNats, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	stream, err := js.CreateOrUpdateStream(ctxNats, jetstream.StreamConfig{
+		Name:     bus.TelemetryStreamName,
+		Subjects: []string{bus.TelemetryStreamSubject},
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// retrieve consumer handle from a stream
+	cons, err := stream.CreateOrUpdateConsumer(ctxNats, jetstream.ConsumerConfig{
+		Durable:        "protoproxy",                         // + hash of pod for scaling?
+		FilterSubjects: []string{bus.TelemetryConsumerProto}, // can filter on specific functions
+		// Implicit for telemerty, explicity for commands and telemetry
+		AckPolicy: jetstream.AckExplicitPolicy,
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	cc, err := cons.Consume(func(msg jetstream.Msg) {
+		// TODO
+		// deserialize from protobuf to line protocol
+		// send to database
+		fmt.Println("Received jetstream message: ", string(msg.Data()))
+		b.Publish(msg.Reply(), []byte("im back!"))
+		msg.Ack()
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer cc.Stop()
+
+	// Simple Async Subscriber
+	// b.Subscribe(">", func(m *nats.Msg) {
+	// 	fmt.Printf("Received a message: %s\n", string(m.Data))
+	// 	b.Publish(m.Reply, []byte("I can help!"))
+	// 	// Header
+	// 	// __msg: <proto_message> to id the message
+	// 	// __id: <device_id> to id the device
+	// 	// the topic will be <device_id>.<type>.<version>
+	// 	// eg. ff123b9.tlm.v1alpha
+	// 	// crazy idea, the proto_message could be the key.
+	// 	// m.Header.Get(key string)
+	// })
+	//
 	// Metrics & Health
 	metrics.RegisterRoutes(mux)
 	health.RegisterRoutes(mux)
@@ -130,8 +196,8 @@ func main() {
 		defer cancel()
 
 		go func() {
-			nc.Drain()
-			nc.Close()
+			b.Drain()
+			b.Close()
 		}()
 
 		if err := server.Shutdown(ctx); err != nil {
