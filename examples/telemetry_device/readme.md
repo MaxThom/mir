fc5a1baa54507dc5d78bf4d3a481e6ee2759cc90
maxthom <maxthomassin@hotmail.com>
2024-08-15 00:48:12 -0400

    Added schema upload via cli

--- examples/telemetry_device/readme.md
+++ examples/telemetry_device/readme.md
@@ -13,3 +13,5 @@ protoc --go_out=. --go_opt=paths=source_relative --descriptor_set_out=example.pb
 descriptorpb: Best for schema inspection and working with the serialized form of descriptors.
 reflect: Ideal for runtime manipulation and introspection of protobuf messages.
 protodesc: Converts between descriptorpb.FileDescriptorProto messages and the reflective protoreflect.FileDescriptor.
+
+protoc command.proto telemetry.proto utils.proto --descriptor_set_out=./gen/schema.bproto --include_imports
--- internal/ui/cli/device_schema_cmd.go
+++ internal/ui/cli/device_schema_cmd.go
@@ -1,14 +1,29 @@
 package cli
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/maxthom/mir/internal/clients/core_client"
+	bus "github.com/maxthom/mir/internal/libs/external/natsio"
+	"github.com/maxthom/mir/pkgs/api/proto/v1alpha/core_api"
+	"github.com/maxthom/mir/pkgs/mir_models"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protodesc"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/types/descriptorpb"
+)
+
 type DeviceSchemaCmd struct {
-	Upload  SchemaUploadCmd  `cmd:"" help:"Upload schema of a set of devices"`
+	Upload  SchemaUploadCmd  `cmd:"" help:"Upload schema of a set of devices. To generate: 'protoc <proto_files...> --descriptor_set_out=./<file_name>.bproto --include_imports'"`
 	Explore SchemaExploreCmd `cmd:"" help:"Explore a device schema"`
 }
 
 type SchemaUploadCmd struct {
+	Output string `short:"o" help:"output format for response" default:"json"`
 	Target `embed:"" prefix:"target."`
 	// TODO could be an array, I think there is a path type
-	Path string `help:"Path to protobuf schema"`
+	Path string `type:"path" help:"Path to protobuf schema"`
 }
 
 type SchemaExploreCmd struct {
@@ -40,7 +55,85 @@ func (d *SchemaUploadCmd) Validate() error {
 }
 
 func (d *SchemaUploadCmd) Run(c CLI) error {
+	var err error
+	msgBus, err := bus.New(c.Target)
+	if err != nil {
+		e := MirConnectionError{Target: c.Target, e: err}
+		fmt.Println(e)
+		return e
+	}
+	defer msgBus.Close()
 
+	schemaData, err := os.ReadFile(d.Path)
+	if err != nil {
+		e := MirProcessError{e: err}
+		fmt.Println(e)
+		return e
+	}
+
+	pbSet := new(descriptorpb.FileDescriptorSet)
+	if err := proto.Unmarshal(schemaData, pbSet); err != nil {
+		e := MirProcessError{e: err}
+		fmt.Println(e)
+		return e
+	}
+	compSch, err := mir_models.CompressFileDescriptorSet(pbSet)
+	if err != nil {
+		e := MirProcessError{e: err}
+		fmt.Println(e)
+		return e
+	}
+	reg, err := protodesc.NewFiles(pbSet)
+	if err != nil {
+		e := MirProcessError{e: err}
+		fmt.Println(e)
+		return e
+	}
+
+	packNames := []string{}
+	reg.RangeFiles(func(f protoreflect.FileDescriptor) bool {
+		packNames = append(packNames, string(f.FullName()))
+		return true
+	})
+
+	req := &core_api.UpdateDeviceRequest{
+		Targets: &core_api.Targets{
+			Ids:         d.Target.Ids,
+			Names:       d.Target.Names,
+			Namespaces:  d.Target.Namespaces,
+			Labels:      d.Target.Labels,
+			Annotations: d.Target.Anno,
+		},
+		Status: &core_api.UpdateDeviceRequest_Status{
+			Schema: &core_api.UpdateDeviceRequest_Schema{
+				CompressedSchema: compSch,
+				PackageNames:     packNames,
+			},
+		},
+	}
+	resp, err := core_client.PublishDeviceUpdateRequest(msgBus, req)
+	if err != nil {
+		e := MirRequestError{Route: "device.update", e: err}
+		fmt.Println(e)
+		return e
+	}
+	if resp.GetError() != nil {
+		e := MirResponseError{
+			Route: "device.update",
+			e: MirHttpError{
+				Code:    resp.GetError().GetCode(),
+				Message: resp.GetError().GetMessage(),
+				Details: resp.GetError().GetDetails(),
+			}}
+		fmt.Println(e)
+		return e
+	}
+	if out, e := MarhsalResponse(d.Output, resp.GetOk()); e != nil {
+		fmt.Println(e)
+		return e
+	} else {
+		fmt.Println(out)
+	}
 	return nil
 }
 
