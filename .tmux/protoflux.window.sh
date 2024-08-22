5378ef807e258f62c94c7e7cbd3056bf5d5c9bfb
maxthom <maxthomassin@hotmail.com>
2024-08-22 11:44:07 -0400

    Added tmux layouts to repo for ergonomics

---
+++ .tmux/protoflux.window.sh
@@ -0,0 +1,26 @@
+# Set window root path. Default is `$session_root`.
+# Must be called before `new_window`.
+window_root "~/code/go/mir/cmds/protoflux/"
+
+# Create new window. If no argument is given, window name will be based on
+# layout file name.
+new_window "protoflux"
+
+#run_cmd "sleep 5 && go run main.go"
+run_cmd 'bash -c "(sleep 5 && cd ../../ && air -c .air/protoflux.toml)"'
+
+
+# Split window into panes.
+split_v 20
+#split_h 50
+
+# Run commands.
+#run_cmd "top"     # runs in active pane
+#run_cmd "date" 1  # runs in pane 1
+
+# Paste text
+#send_keys "top"    # paste into active pane
+#send_keys "date" 1 # paste into pane 1
+
+# Set active pane.
+#select_pane 0
