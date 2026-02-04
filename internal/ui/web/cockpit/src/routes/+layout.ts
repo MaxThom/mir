f811f24d74bb204e179abb668aefe9a0924ee4a7
MaxThom <maxthomassin@hotmail.com>
2026-02-04 10:24:56 -0600

    added default svelte project with shadcn

---
+++ internal/ui/web/cockpit/src/routes/+layout.ts
@@ -0,0 +1,5 @@
+// Enable prerendering for the entire app (required for adapter-static)
+export const prerender = true;
+
+// This makes the app behave as a Single Page Application (SPA)
+export const ssr = false;
