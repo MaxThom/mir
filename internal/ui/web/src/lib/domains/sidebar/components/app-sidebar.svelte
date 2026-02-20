0e9abecebac3a483e28f285e18360442db970859
MaxThom <maxthomassin@hotmail.com>
2026-02-20 17:35:17 -0500

    adjusted side menu a bit

--- internal/ui/web/src/lib/domains/sidebar/components/app-sidebar.svelte
+++ internal/ui/web/src/lib/domains/sidebar/components/app-sidebar.svelte
@@ -24,14 +24,21 @@
 
 <Sidebar.Root {collapsible} {...restProps}>
 	<Sidebar.Header>
-		<ContextSwitcher />
+		<div class="flex flex-col gap-0">
+			<Sidebar.GroupLabel class="group-data-[collapsible=icon]:hidden">Contexts</Sidebar.GroupLabel>
+			<ContextSwitcher />
+		</div>
 	</Sidebar.Header>
 	<Sidebar.Content>
 		<NavSection label="Platform" items={navMain} />
 	</Sidebar.Content>
 	<Sidebar.Footer>
-		<div class="flex items-center justify-center">
-			<img src={MirAlphaLogo} alt="Mir Logo" class="w-48 items-center justify-center" />
+		<div class="flex flex-col items-center justify-center group-data-[collapsible=icon]:hidden">
+			<img src={MirAlphaLogo} alt="Mir Logo" class="w-48" />
+			<span class="text-sm font-semibold tracking-widest text-sidebar-foreground/40 uppercase">Mir Cockpit</span>
+		</div>
+		<div class="hidden items-center justify-center group-data-[collapsible=icon]:flex">
+			<img src={MirAlphaLogo} alt="Mir Logo" class="w-8" />
 		</div>
 		<NavUser {user} />
 	</Sidebar.Footer>
